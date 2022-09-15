7331ab09b49a4d68d44f897877d0189b42c7c63b
guilherminh0 <105890877+guilherminh0@users.noreply.github.com>
2022-09-15 19:50:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+var blockImageObject = "";
+function new_image()
+{
+    fabric.Image.fromURL('AniversarioImagem.png', function(Img) {
+    blockImageObject = Img;
+    blockImageObject.scaleToWidth(700);
+    blockImageObject.scaleToHeight(510);
+    blockImageObject.set({
+    top:0,
+    left:0
+    });
+    canvas.add(blockImageObject);
+    } )
+}
+
+function playSound(){
+    x.play();
+}
